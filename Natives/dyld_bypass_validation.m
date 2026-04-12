00678b07a192ef5c79f8c4a2e4cecf1d7406c8c5
khanhduytran0 <khanhduytran0@users.noreply.github.com>
2026-04-12 21:14:11 +0700

    fix: hooked_mmap should revalidate memory prot
    Fix libnetty not loading with correct memory protections causing 1.21.11 to crash when querying server info. (iOS 18.x and below)

--- Natives/dyld_bypass_validation.m
+++ Natives/dyld_bypass_validation.m
@@ -159,8 +159,13 @@ void* hooked_mmap(void *addr, size_t len, int prot, int flags, int fd, off_t off
     }
     
     void *map = __mmap(addr, len, prot, flags, fd, offset);
+    // Handle some cases where it will still map but without executable permission
+    if (mprotect(map, len, prot) == -1) {
+        munmap(map, len);
+        map = MAP_FAILED;
+    }
     if (map == MAP_FAILED && fd && (prot & PROT_EXEC)) {
-        printf("[DyldLVBypass] mmap(prot=%d, flags=%d, fd=%d)\n", prot, flags, fd);
+        //printf("[DyldLVBypass] mmap(prot=%d, flags=%d, fd=%d)\n", prot, flags, fd);
         map = __mmap(addr, len, prot, flags | MAP_PRIVATE | MAP_ANON, 0, 0);
         if (DeviceHasJITFlags(JIT_FLAG_FORCE_MIRRORED | JIT_FLAG_HAS_TXM)) {
             JIT26PrepareRegion(map, len);
